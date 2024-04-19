00d4d171feb84626769033d51f37cdf77d4244ac
tomcli <tomcli@tencent.com>
2024-04-19 14:32:09 +0800

    leetcode lazy loading

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,6 +1,8 @@
+local leet_arg = "leetcode.nvim"
 return {
   "kawre/leetcode.nvim",
   build = ":TSUpdate html",
+  lazy = leet_arg ~= vim.fn.argv()[1],
   dependencies = {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim", -- required by telescope
@@ -13,6 +15,13 @@ return {
   },
   opts = {
     -- configuration goes here
+    arg = leet_arg,
+    lang = "golang",
+    injector = {
+      ["golang"] = {
+        before = { "package leetcode" },
+      },
+    },
     cn = {
       enabled = true,
       translator = true,
